9f72829584c0e1a091882b2698c0872a95b8ca78
Darshan Kumar T <darshan_kumar_t@outlook.com>
2023-09-24 07:59:35 +0530

    Delete take 2 directory

--- take 2/welcome.js
+++
@@ -1,9 +0,0 @@
-// Add JavaScript functionality for the button
-document.addEventListener("DOMContentLoaded", function () {
-    const mainButton = document.getElementById("mainButton");
-    
-    mainButton.addEventListener("click", function () {
-        // Replace 'main-webpage.html' with the actual URL of your main webpage
-        window.location.href = "main.html";
-    });
-});
